413506ca4722f43c2f9241879ac7b44ee8940768
a52sinha <a52sinha@uwaterloo.ca>
2023-11-23 16:55:10 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-og_word = "Hello"
+og_word = "Tulell"
 og_wordlist= list(og_word)
 
 
@@ -9,16 +9,28 @@ for i in range(len(og_wordlist)):
     while (index_input < 0 or index_input > (len(og_word)-1)):
         print ("Please enter an integer from within the range")
         index_input = int(input("Enter which letter place you wish to guess between 0-" + str(len(og_word)-1) + ": "))
-
-    if 0 <= index_input <=len(og_word)-1:
-        character_input = input ("Choose your guess for this place: ")
-        if character_input == og_wordlist[index_input]:
-            print ("Correct!") # add graphics here like dashes etc...
-        else:
-            print ("incorrect")
+        
+    temp = 0
+    while temp == 0:
+        if 0 <= index_input <=len(og_word)-1:
+            character_input = input ("Choose your guess for this place: ")
+            if character_input == og_wordlist[index_input]:
+                print ("Correct!") # add graphics here like dashes etc...
+                temp +=1
+            else:
+                print ("incorrect")
+                
 # to do:
     #error trap for str and flopat
     # if character input is not str
     # limit gueses for each place + character 
     # can't re guess for same plavce after correct 
     # kat graphics -= dasdhes and hangman 
+      
+#wieuhruhyiu2hr
+    
+
+      
+
+
+
