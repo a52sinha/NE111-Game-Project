01e0c66700a1d5c46c45673ce6b941ac7da151cc
a52sinha <a52sinha@uwaterloo.ca>
2023-11-28 13:03:15 -0500

    New changes
    while loop

--- game.py
+++ game.py
@@ -16,25 +16,32 @@ for i in range(len(og_wordlist)):
     
     b=0
     while b == 0:
-        if index_input.isnumeric()== True: # makes sure every variable entered is a letter 
+        if index_input.isnumeric(): # makes sure every variable entered is a letter 
             index_input = int(index_input)
-            while (index_input < 0 or index_input > (len(og_word)-1)):
-                print ("Your input is not valid. Please enter an integer from within the range")
-                index_input = int(input("Enter which letter place you wish to guess between 0-" + str(len(og_word)-1) + ": "))       
             b+=1
         else:
-            print("Your input is not valid. Please enter an integer from within the range")
+            print("Your input is not valid. Please enter a number")
             index_input = input("Enter which letter place you wish to guess between 0-" + str(len(og_word)-1) + ": ")
-  
+
+    while (index_input < 0 or index_input > (len(og_word)-1)):
+        print ("Please enter an integer from within the range")
+        index_input = int(input("Enter which letter place you wish to guess between 0-" + str(len(og_word)-1) + ": "))
+        
     temp = 0
     while temp > -6:
         if 0 <= index_input <=len(og_word)-1:
-            character_input = input ("Choose your guess for this place: ")
+            c=0
+            while c == 0:
+                character_input = input ("Choose one letter to guess for this place: ")
+                if character_input.isalpha() and len(character_input) == 1: # makes sure every variable entered is a letter 
+                    c+=1
+                else:
+                    print("Your input is not valid ") # informs the player what they have entered is not valid and tells them to enter a new one        
             if character_input == og_wordlist[index_input]:
                 print ("Correct!") # add graphics here like dashes etc...
                 break 
             else:
-                print ("incorrect")
+                print ("Incorrect :(")
                 temp-=1
     if temp <= -6:
         print ("Game over :(")  
@@ -44,14 +51,10 @@ for i in range(len(og_wordlist)):
  
                 
                 
-# to do:
-    #error trap for str and flopat
-    # if character input is not str
-    # limit gueses for each place + character 
+# to do:x
     # can't re guess for same plavce after correct 
     # kat graphics -= dasdhes and hangman 
       
-#wieuhruhyiu2hr
     
 
       
